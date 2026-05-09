b6ded8712ac3757d6f432e5db278f3399292e3fb
AhmedReda178 <rda49586@gmail.com>
2026-05-09 22:33:57 +0300

    class Transaction

---
+++ BankAccount/Part01-Classes/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount.Part01_Classes
+{
+    public enum TransactionType { Deposit, Withdrawal, Transfer, Payment }
+
+    public class Transaction
+    {
+        public int TransactionNumber { get; set; }   // PK
+        public DateTime TransactionDate { get; set; }
+        public decimal Amount { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public string Note { get; set; }
+
+        // FK
+        public string AccountNumber { get; set; }
+        public Account Account { get; set; }
+    }
+}
