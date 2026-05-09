3b01f46ba188686a97fd406405e583f22ef39097
AhmedReda178 <rda49586@gmail.com>
2026-05-09 22:32:28 +0300

    class CustomerAccount

---
+++ BankAccount/Part01-Classes/CustomerAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount.Part01_Classes
+{
+    public enum OwnershipType { Primary, CoHolder }
+    public enum AccountStatus { Active, Closed }
+
+    public class CustomerAccount
+    {
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public string AccountNumber { get; set; }
+        public Account Account { get; set; }
+
+        public DateTime OwnershipStartDate { get; set; }
+        public OwnershipType OwnershipType { get; set; }
+        public AccountStatus AccountStatus { get; set; }
+    }
+}
